55f370a1e0f8b96ed54130d3c17f98a4fd1f89ac
David Higgins <dhiggins@aimmedia.com>
2014-12-03 12:27:23 -0500

    added "active" class to top-level cats when in sub-cat

--- wp_bootstrap_navwalker.php
+++ wp_bootstrap_navwalker.php
@@ -67,6 +67,8 @@ class wp_bootstrap_navwalker extends Walker_Nav_Menu {
 
 			if ( in_array( 'current-menu-item', $classes ) )
 				$class_names .= ' active';
+			if ( in_array( 'current-category-ancestor', $classes ) ) 
+				$class_names .= ' active';
 
 			$class_names = $class_names ? ' class="' . esc_attr( $class_names ) . '"' : '';
 
